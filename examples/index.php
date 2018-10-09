5a7da24ddd37026d4a96ac199e1121b8591d37b9
robinmglsk <robin@migalski.be>
2018-10-09 14:02:58 +0200

    Added docker to run examples

---
+++ examples/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Examples</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1>Examples</h1>
+        <ul class="list-group">
+            <li class="list-group-item">
+                <h3><a href="example01.php">Example 01</a></h3>
+                <p>Render html form</p>
+            </li>
+            <li class="list-group-item">
+                <h3><a href="example02.php">Example 02</a></h3>
+                <p>Get db fields</p>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
